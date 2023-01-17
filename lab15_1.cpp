cd34363c882bca8b579a47ec07cd70d0ea3c75df
aeiout <ueaarthon13117@gmail.com>
2023-01-17 17:39:49 +0700

    completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 using namespace std;
-
+template <typename T>
+void show(T d[],int N){
+	for(int i=0;i<N;i++){
+		cout<<d[i]<<" ";
+	}
+	cout<<"\n";
+}
 template <typename T>
 void insertionSort(T d[],int N){
+	T x;
+	for(int i=1;i<N;i++){
+		x=d[i];
+		
+		for(int j=i-1;j>=0;j--){
+			if(d[j+1]>d[j]){
+				x=d[j];
+				d[j]=d[j+1];
+				d[j+1]=x;
+			}
+		}
+		cout<<"Pass "<<i<<":";
+		show(d,N);
+	}
 
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +38,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
